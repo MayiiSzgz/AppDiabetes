cd5c3a0372d5a7d5ee156c911b0355e0bcb646c3
Mayii <marysg98@yahoo.com>
2023-05-08 15:14:16 -0600

    agregue pantalla de carga

---
+++ app/src/main/java/com/example/appdiabetes/pantallaCargaActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdiabetes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallaCargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+    }
+}
