0bda596cc636f568b07e1aa71bcade1cfb9a18ee
JoLuG <jorgonzsu025@outlook.com>
2023-06-28 18:09:55 -0600

    Prueba alarma

--- app/src/main/java/com/example/appdiabetes/MyForegroundService.java
+++ app/src/main/java/com/example/appdiabetes/MyForegroundService.java
@@ -19,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MyForegroundService extends Service {
@@ -60,22 +61,76 @@ public class MyForegroundService extends Service {
             Query query = db.collection("Medicamentos")
                     .document(user.getUid())
                     .collection("Historial")
-                    .orderBy("fechaHora", Query.Direction.DESCENDING)
-                    .limit(1);
+                    .orderBy("siguienteDosis", Query.Direction.ASCENDING)
+                    .limit(2);
 
             query.get().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     List<DocumentSnapshot> documents = task.getResult().getDocuments();
-                    if (!documents.isEmpty()) {
+                    if (documents.size() >= 2) {
+                        DocumentSnapshot firstDose = documents.get(0);
+                        DocumentSnapshot secondDose = documents.get(1);
+
+                        long currentTime = Calendar.getInstance().getTimeInMillis();
+
+                        // Obtener los valores de siguienteDosis como cadenas de texto
+                        String firstDoseTimeStr = firstDose.getString("siguienteDosis");
+                        String secondDoseTimeStr = secondDose.getString("siguienteDosis");
+
+                        // Convertir las cadenas de texto a valores numéricos
+                        long firstDoseTime = convertTimeStringToTimestamp(firstDoseTimeStr);
+                        long secondDoseTime = convertTimeStringToTimestamp(secondDoseTimeStr);
+
+                        // Comparar los tiempos de las dosis con la hora actual
+                        if (firstDoseTime >= currentTime) {
+                            // La primera dosis es la más cercana a la hora actual
+                            long timeDifference = firstDoseTime - currentTime;
+                            if (timeDifference <= 30 * 60 * 1000) {
+                                // La siguiente dosis está a menos de 30 minutos de distancia
+                                String siguienteDosis = firstDose.getString("siguienteDosis");
+                                showNotification(siguienteDosis);
+                            }
+                        } else if (secondDoseTime >= currentTime) {
+                            // La segunda dosis es la más cercana a la hora actual
+                            long timeDifference = secondDoseTime - currentTime;
+                            if (timeDifference <= 30 * 60 * 1000) {
+                                // La siguiente dosis está a menos de 30 minutos de distancia
+                                String siguienteDosis = secondDose.getString("siguienteDosis");
+                                showNotification(siguienteDosis);
+                            }
+                        }
+                    } else if (documents.size() == 1) {
+                        // Solo hay una dosis programada
                         DocumentSnapshot document = documents.get(0);
                         String siguienteDosis = document.getString("siguienteDosis");
-
-                        // Mostrar la siguiente dosis en la barra de notificaciones
                         showNotification(siguienteDosis);
                     }
                 }
             });
         }
+    }
+
+
+    private long convertTimeStringToTimestamp(String timeString) {
+        try {
+            // Obtener la hora y los minutos de la cadena de texto
+            String[] parts = timeString.split(":");
+            int hour = Integer.parseInt(parts[0]);
+            int minute = Integer.parseInt(parts[1]);
+
+            // Obtener el calendario actual y establecer la hora y los minutos
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
+
+            // Obtener el valor numérico en milisegundos desde la medianoche
+            return calendar.getTimeInMillis();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0; // Devuelve 0 si hay algún error al convertir la cadena de texto
     }
 
     private void showNotification(String siguienteDosis) {
@@ -108,6 +163,4 @@ public class MyForegroundService extends Service {
                 .setContentIntent(pendingIntent)
                 .build();
     }
-
 }
-
