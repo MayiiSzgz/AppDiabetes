af6d9d200a504aeb7b7f38ce6dccf2c241c99e54
JoLuG <jorgonzsu025@outlook.com>
2023-06-29 01:16:55 -0600

    Alarma al 70% xddd

--- app/src/main/java/com/example/appdiabetes/StopReminderReceiver.java
+++ app/src/main/java/com/example/appdiabetes/StopReminderReceiver.java
@@ -1,33 +1,38 @@
 package com.example.appdiabetes;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.IBinder;
 import android.os.Vibrator;
 
-public class StopReminderReceiver extends BroadcastReceiver {
+public class StopReminderReceiver extends Service {
 
     private MediaPlayer mediaPlayer;
     private Vibrator vibrator;
 
-    public void setMediaPlayer(MediaPlayer mediaPlayer) {
-        this.mediaPlayer = mediaPlayer;
-    }
-
-    public void setVibrator(Vibrator vibrator) {
-        this.vibrator = vibrator;
-    }
-
     @Override
-    public void onReceive(Context context, Intent intent) {
-        // Detener la reproducción del sonido y la vibración
+    public void onCreate() {
+        super.onCreate();
+        // Detener la reproducción del sonido
         if (mediaPlayer != null && mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
             mediaPlayer.release();
+            mediaPlayer = null;
         }
+
+        // Detener la vibración
         if (vibrator != null) {
             vibrator.cancel();
+            vibrator = null;
         }
+
+        // Detener el servicio
+        stopSelf();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
